0340a551eb8e34674b5d3c06575047a73aa29051
Matthew Curry <matt.curry56@gmail.com>
2022-09-02 19:13:05 -0400

    initial frontend commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>RetireRight</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+    
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
