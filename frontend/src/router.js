f404756c92020933697208e2816bc290b7fd2945
Matthew Curry <matt.curry56@gmail.com>
2022-09-06 09:44:38 -0400

    implicit auth flow works

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -39,7 +39,7 @@ export const router = createRouter({
         },
         {
             path: '/login', beforeEnter(to, from, next) {
-                auth.auth.getSession();
+                auth.login();
             }
         },
 
